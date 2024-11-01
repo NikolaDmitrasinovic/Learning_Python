5a81460038ced5de636f51f9a8a8a9cf8cab97a9
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-11-01 12:19:17 +0100

    chore: add conditionals and imports

---
+++ python3fundamentals/2_ConditionalsAndImports/weather.py
@@ -0,0 +1,17 @@
+# temperature = 75
+# forecast = "rainy"
+
+# if temperature > 80:
+#     print("It's too hot!")
+#     print("Stay inside!")
+# elif temperature < 60 and forecast != "rainy":
+#     print("Enjoy the outdoors!")
+# else:
+#     print("It's a bit cold!")
+
+rainy = False
+
+if rainy:
+    print("Stay inside, it's rainy!")
+else:
+    print("Enhoy the outdoors!")
